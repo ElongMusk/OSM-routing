099056802d6230622251a671ddafc999309e7dc0
ElongMusk <71707464+ElongMusk@users.noreply.github.com>
2022-09-07 16:19:49 +0100

    Add project files.

---
+++ OSM routing/Program.cs
@@ -0,0 +1,61 @@
+﻿using Itinero;
+using Itinero.IO.Osm;
+using Itinero.Osm.Vehicles;
+using System;
+using System.IO;
+
+namespace OSM_routing
+{
+    class Program
+    {
+        public static string SourceDataPFB = "C:\\Users\\639519\\source\\repos\\OSM routing\\OSM routing\\east-yorkshire-with-hull-latest.osm.pbf";
+        public static string DBFile = "uni.routerdb";
+        public static string GeoJSON = "route.geojson";
+
+        static void Main()
+        {
+            CreateDB();
+
+            
+
+            RouterDb routerDb = null;
+            using (var stream = new FileInfo(DBFile).OpenRead())
+            {
+                routerDb = RouterDb.Deserialize(stream);
+            }
+            var router = new Router(routerDb);
+
+            // get a profile.
+            var profile = Vehicle.Pedestrian.Fastest(); // the default OSM pedestian profile.
+
+            // create a routerpoint from a location.
+            // snaps the given location to the nearest routable edge.
+            var start = router.Resolve(profile, 53.769153f, -0.368449f);
+            var end = router.Resolve(profile, 53.771634f, -0.366966f);
+
+            // calculate a route.
+            var route = router.Calculate(profile, start, end);
+            using (var writer = new StreamWriter(@GeoJSON))
+            {
+                route.WriteGeoJson(writer);
+            }
+        }
+
+
+        public static void CreateDB()
+        {
+            var routerDb = new RouterDb();
+            using (var stream = new FileInfo(SourceDataPFB).OpenRead())
+            {
+                // create the network for cars.
+                routerDb.LoadOsmData(stream, Vehicle.Pedestrian);
+            }
+
+            // write the routerdb to disk.
+            using (var stream = new FileInfo(@DBFile).Open(FileMode.Create))
+            {
+                routerDb.Serialize(stream);
+            }
+        }
+    }
+}
